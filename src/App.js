f96cb29bedb0e236e2e089b256687912cab956c0
farabi1 <rashidfarabi@gmail.com>
2022-05-22 00:56:19 +0600

    Client Setup- Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Twelve Assignment Manufacturer</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
