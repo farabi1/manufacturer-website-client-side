a1edc44832d052a386f6346760211e4923870eb1
farabi1 <rashidfarabi@gmail.com>
2022-05-22 20:52:05 +0600

    Client Setup - Google login

--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Footer from '../../../Components/Footer/Footer';
 import Header from '../../../Components/Header/Header';
-
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const Login = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
         <div>
             <Header></Header>
-            <h1>Login</h1>
+
+            {/* -------------- */}
+
+            <div className="flex h-screen justify-center items-center">
+                <div className="card w-96 bg-slate-50 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="text-center text-2xl font-bold text-black">Login</h2>
+
+                        <div className="divider text-black font-bold text-sm">OR</div>
+                        <button
+                            onClick={() => signInWithGoogle()}
+                            className="btn btn-outline btn-success text-lg"
+                        >Continue With Google</button>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* ------------------ */}
+
             <Footer></Footer>
         </div>
     );
