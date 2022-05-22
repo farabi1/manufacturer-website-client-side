476062c960e49a5e1c04eb2010416318b71d4694
farabi1 <rashidfarabi@gmail.com>
2022-05-22 17:25:57 +0600

    Client Setup - Hero

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../../Components/Banner/Banner';
 import Header from '../../Components/Header/Header';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
         </div>
     );
 };
