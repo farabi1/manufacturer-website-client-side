c7dd13225a116552ce7fdfe2fd20989e85e0a2c9
farabi1 <rashidfarabi@gmail.com>
2022-05-24 16:22:21 +0600

    Client Setup - Reviews

--- src/Components/Auth Nested Route/User/MyReviews/AddReviews.js
+++ src/Components/Auth Nested Route/User/MyReviews/AddReviews.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 
 const AddReviews = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
-            <h1>ff</h1>
+            <h1>Add a Review</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Name" className="input input-bordered input-accent w-full max-w-xs" {...register("name", { required: true, maxLength: 20 })} />
+                <input type="text" placeholder="Review" className="input input-bordered input-accent w-full max-w-xs my-6" {...register("text", { required: true, maxLength: 20 })} />
+                <input type="text" placeholder="Rating" className="input input-bordered input-accent w-full max-w-xs" {...register("rate", { required: true, maxLength: 20 })} />
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
