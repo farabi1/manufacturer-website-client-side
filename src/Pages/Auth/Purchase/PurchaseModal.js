6e7998e8a3f8ca7b535d627c03547cefde2e8baa
farabi1 <rashidfarabi@gmail.com>
2022-05-25 00:33:33 +0600

    Client Setup - Modal & Buy Now info

--- src/Pages/Auth/Purchase/PurchaseModal.js
+++ src/Pages/Auth/Purchase/PurchaseModal.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const PurchaseModal = ({ purchase }) => {
 
-    console.log(purchase);
     const { name, price } = purchase || {};
+
+    const handlePurchase = event => {
+        event.preventDefault();
+        console.log(name);
+    }
 
     return (
         <div>
@@ -12,11 +16,11 @@ const PurchaseModal = ({ purchase }) => {
                 <div className="modal-box">
                     <label for="purchase-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-primary">Customer Name : {name}</h3>
-                    <form className='grid grid-cols-1 gap-4 mt-3'>
-                        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" />
-                        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" />
+                    <form onSubmit={handlePurchase} className='grid grid-cols-1 gap-4 mt-3'>
+                        <input type="text" readOnly value={price} class="input input-bordered input-success w-full max-w-xs" />
+                        <input type="text" name="name" placeholder="Your Name" class="input input-bordered input-success w-full max-w-xs" />
+                        <input type="email" name="email" placeholder="Email address" class="input input-bordered input-success w-full max-w-xs" />
+                        <input type="text" name="address" placeholder="Home Address" class="input input-bordered input-success w-full max-w-xs" />
                         <input type="submit" value="submit" placeholder="Type here" class="btn btn-secondary input-success w-full max-w-xs" />
                     </form>
 
