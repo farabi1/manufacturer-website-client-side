e2d781279fb446337f1cbec4f0eeacf136e7e221
farabi1 <rashidfarabi@gmail.com>
2022-05-26 21:58:02 +0600

    Client Setup -Manage All Order  Setup

--- src/Components/Auth Nested Route/Admin/Manage All Orders/ManageAllOrders.js
+++ src/Components/Auth Nested Route/Admin/Manage All Orders/ManageAllOrders.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const ManageAllOrders = () => {
+    const [allorders, setAllorders] = useState([]);
+    useEffect(() => {
+
+        fetch(`https://immense-shore-60421.herokuapp.com/orders`)
+            .then(res => res.json())
+            .then(data => { setAllorders(data) })
+
+
+    }, [])
+
+    const handlePending = () => {
+        toast.success(`Orders is approved`);
+
+    }
     return (
         <div>
-            <h1>manage</h1>
+
+            <h1>All order List: {allorders.length}</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Product</th>
+                            <th>Address</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allorders.map((allorder, index) =>
+                                <tr key={index} >
+                                    <th>{index + 1}</th>
+                                    <td>{allorder.customerName}</td>
+                                    <td>{allorder.customerMail}</td>
+                                    <td>{allorder.purchase}</td>
+                                    <td>{allorder.address}</td>
+                                    <td><button onClick={handlePending} className="btn btn-outline btn-success btn-sm" >Pending</button></td>
+                                </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
